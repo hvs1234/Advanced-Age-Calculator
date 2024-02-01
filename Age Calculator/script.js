d6b4765b3b9584a13d5f3f7fbec46d53f50b4238
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2024-02-01 20:29:00 +0530

    Update script.js

--- Age Calculator/script.js
+++ Age Calculator/script.js
@@ -45,3 +45,241 @@ btn.addEventListener('click', () => {
 getDaysInMonth = (year,month) => {
     return new Date(year,month,0).getDate();
 };
+
+/*
+const input = document.querySelector('#input');
+input.max = new Date().toISOString().split('T')[0];
+const btn = document.querySelector('#btn');
+const result = document.querySelector('#result');
+
+btn.addEventListener('click', () => {
+    let birthdate = new Date(input.value);
+    let d1 = birthdate.getDate();
+    let m1 = birthdate.getMonth() + 1;
+    let y1 = birthdate.getFullYear();
+    
+    let today = new Date();
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+
+    let d3,m3,y3;
+    y3 = y2 - y1;
+
+    if(m2>=m1)
+    {
+        m3 = m2 - m1;
+    } else{
+        y3--;
+        m3 = 12 + m2 - m1;
+    }
+
+    if(d2>=d1)
+    {
+        d3 = d2 - d1;
+    } else {
+        m3--;
+        d3 = getDaysInMonth(y1,m1) + d2 - d1;
+    }
+
+    if(m3 < 0)
+    {
+        m3 = 11;
+        y3--;
+    }
+    result.innerHTML = `You are ${y3} years, ${m3} months and ${d3} days old`;
+
+});
+
+getDaysInMonth = (year,month) => {
+    return new Date(year,month,0).getDate();
+};
+const input = document.querySelector('#input');
+input.max = new Date().toISOString().split('T')[0];
+const btn = document.querySelector('#btn');
+const result = document.querySelector('#result');
+
+btn.addEventListener('click', () => {
+    let birthdate = new Date(input.value);
+    let d1 = birthdate.getDate();
+    let m1 = birthdate.getMonth() + 1;
+    let y1 = birthdate.getFullYear();
+    
+    let today = new Date();
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+
+    let d3,m3,y3;
+    y3 = y2 - y1;
+
+    if(m2>=m1)
+    {
+        m3 = m2 - m1;
+    } else{
+        y3--;
+        m3 = 12 + m2 - m1;
+    }
+
+    if(d2>=d1)
+    {
+        d3 = d2 - d1;
+    } else {
+        m3--;
+        d3 = getDaysInMonth(y1,m1) + d2 - d1;
+    }
+
+    if(m3 < 0)
+    {
+        m3 = 11;
+        y3--;
+    }
+    result.innerHTML = `You are ${y3} years, ${m3} months and ${d3} days old`;
+
+});
+
+getDaysInMonth = (year,month) => {
+    return new Date(year,month,0).getDate();
+};
+const input = document.querySelector('#input');
+input.max = new Date().toISOString().split('T')[0];
+const btn = document.querySelector('#btn');
+const result = document.querySelector('#result');
+
+btn.addEventListener('click', () => {
+    let birthdate = new Date(input.value);
+    let d1 = birthdate.getDate();
+    let m1 = birthdate.getMonth() + 1;
+    let y1 = birthdate.getFullYear();
+    
+    let today = new Date();
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+
+    let d3,m3,y3;
+    y3 = y2 - y1;
+
+    if(m2>=m1)
+    {
+        m3 = m2 - m1;
+    } else{
+        y3--;
+        m3 = 12 + m2 - m1;
+    }
+
+    if(d2>=d1)
+    {
+        d3 = d2 - d1;
+    } else {
+        m3--;
+        d3 = getDaysInMonth(y1,m1) + d2 - d1;
+    }
+
+    if(m3 < 0)
+    {
+        m3 = 11;
+        y3--;
+    }
+    result.innerHTML = `You are ${y3} years, ${m3} months and ${d3} days old`;
+
+});
+
+getDaysInMonth = (year,month) => {
+    return new Date(year,month,0).getDate();
+};
+const input = document.querySelector('#input');
+input.max = new Date().toISOString().split('T')[0];
+const btn = document.querySelector('#btn');
+const result = document.querySelector('#result');
+
+btn.addEventListener('click', () => {
+    let birthdate = new Date(input.value);
+    let d1 = birthdate.getDate();
+    let m1 = birthdate.getMonth() + 1;
+    let y1 = birthdate.getFullYear();
+    
+    let today = new Date();
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+
+    let d3,m3,y3;
+    y3 = y2 - y1;
+
+    if(m2>=m1)
+    {
+        m3 = m2 - m1;
+    } else{
+        y3--;
+        m3 = 12 + m2 - m1;
+    }
+
+    if(d2>=d1)
+    {
+        d3 = d2 - d1;
+    } else {
+        m3--;
+        d3 = getDaysInMonth(y1,m1) + d2 - d1;
+    }
+
+    if(m3 < 0)
+    {
+        m3 = 11;
+        y3--;
+    }
+    result.innerHTML = `You are ${y3} years, ${m3} months and ${d3} days old`;
+
+});
+
+getDaysInMonth = (year,month) => {
+    return new Date(year,month,0).getDate();
+};
+const input = document.querySelector('#input');
+input.max = new Date().toISOString().split('T')[0];
+const btn = document.querySelector('#btn');
+const result = document.querySelector('#result');
+
+btn.addEventListener('click', () => {
+    let birthdate = new Date(input.value);
+    let d1 = birthdate.getDate();
+    let m1 = birthdate.getMonth() + 1;
+    let y1 = birthdate.getFullYear();
+    
+    let today = new Date();
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+
+    let d3,m3,y3;
+    y3 = y2 - y1;
+
+    if(m2>=m1)
+    {
+        m3 = m2 - m1;
+    } else{
+        y3--;
+        m3 = 12 + m2 - m1;
+    }
+
+    if(d2>=d1)
+    {
+        d3 = d2 - d1;
+    } else {
+        m3--;
+        d3 = getDaysInMonth(y1,m1) + d2 - d1;
+    }
+
+    if(m3 < 0)
+    {
+        m3 = 11;
+        y3--;
+    }
+    result.innerHTML = `You are ${y3} years, ${m3} months and ${d3} days old`;
+
+});
+
+getDaysInMonth = (year,month) => {
+    return new Date(year,month,0).getDate();
+};
+*/
